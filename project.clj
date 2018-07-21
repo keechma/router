e21a9af0da474a31eef636dce9b1e401aa1ac5f4
Mihael Konjevic <konjevic@gmail.com>
2018-07-21 19:22:52 +0200

    Remove resources folder as it's not needed. Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject keechma/router "0.1.1"
+(defproject keechma/router "0.1.2"
   :description "Router - Pure functional router for ClojureScript applications."
   :url "http://keechma.com/"
   :license {:name "MIT"}
