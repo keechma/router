a009e452100bde3f76f199139821556f23684884
Mihael Konjevic <konjevic@gmail.com>
2022-04-12 17:06:19 +0200

    Switch to shadow-cljs

--- test/router/test.cljs
+++
@@ -1,8 +0,0 @@
-(ns router.test
-  (:require [doo.runner :refer-macros [doo-tests]]
-            [router.router-test]
-            [router.v2.router-test]))
-
-(doo-tests
-  'router.router-test
-  'router.v2.router-test)
